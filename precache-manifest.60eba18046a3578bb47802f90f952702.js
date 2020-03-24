7d21a9906381734dd2d598e6b4b27ebbf4abe040
reguv760 <reguv760@gmail.com>
2020-03-24 11:58:43 -0700

    Updates

---
+++ precache-manifest.60eba18046a3578bb47802f90f952702.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aadd1bd2ed1b8c356670727d5217e060",
+    "url": "/react-oauth-google/index.html"
+  },
+  {
+    "revision": "8145199790c41fd3a519",
+    "url": "/react-oauth-google/static/js/2.59316ce8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-oauth-google/static/js/2.59316ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eec9cfbc07170191529",
+    "url": "/react-oauth-google/static/js/main.0fb63c03.chunk.js"
+  },
+  {
+    "revision": "2e1bc00e3519e5a6f340",
+    "url": "/react-oauth-google/static/js/runtime-main.5a171aeb.js"
+  }
+]);

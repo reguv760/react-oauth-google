e69998d8f5d6377455b910a75d073741508846a5
reguv760 <reguv760@gmail.com>
2020-03-24 12:31:05 -0700

    create new clientId

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import React from "react";
-import GoogleLogin from "react-google-login";
 
-const responseGoogle = response => {
-  console.log(response);
-};
-
-function App() {
-  return (
-    <div className='App'>
-      <GoogleLogin
-        clientId='45511987383-e3qcv4js8kpmuvctin0i59au64efpotv.apps.googleusercontent.com'
-        buttonText='Login'
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={"single_host_origin"}
-      />
-    </div>
-  );
+function App({ children }) {
+  return <div className='App'> {children} </div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import GoogleLogin from "react-google-login";
+
 import App from "./App";
 
+const responseGoogle = response => {
+  console.log(response);
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App>
+    <GoogleLogin
+      clientId='45511987383-hq1pqnli4ca0f5o5ggqab4pvq1t8f7i6.apps.googleusercontent.com'
+      buttonText='Login'
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      cookiePolicy={"single_host_origin"}
+    />
+  </App>,
   document.getElementById("root")
 );

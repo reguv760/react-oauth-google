d49f50257162de139f48c2786e87791f38e21831
reguv760 <reguv760@gmail.com>
2020-03-24 12:11:45 -0700

    change clientId

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className='App'>
       <GoogleLogin
-        clientId='45511987383-6l84afniuqc2aoegdjgqm6l9kjrknfeb.apps.googleusercontent.com'
+        clientId='45511987383-e3qcv4js8kpmuvctin0i59au64efpotv.apps.googleusercontent.com'
         buttonText='Login'
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
